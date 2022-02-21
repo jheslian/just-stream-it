e21efe2ad38bd18a8468f20ffef5bdf819656a08
Jheslian <justinjhes@gmail.com>
2022-02-21 23:25:54 +0100

    add best rated films

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,22 @@
 let films_url = `http://127.0.0.1:8000/api/v1/titles/`
 let genre_url = `http://127.0.0.1:8000/api/v1/genres/`
 
+/**
+ * get data
+ *
+ * @param {string} url
+ * @return {promise} data
+ */
 function fetchUrl(url) {
-    // fetch url
     return fetch(url).then((response) => response.json())
 }
 
+/**
+ * get all categories
+ *
+ * @param {string} url
+ * @param {function} callback
+ */
 const genres_list = [];
 function getGenres(url, callback){
     fetchUrl(url).then((res) => {
@@ -18,17 +29,19 @@ function getGenres(url, callback){
         if(res.next == null){
             callback.call(this);
         }    
-    })   
-    
+    })     
 }    
+
+/*******************************     Add categories     *******************************/
+/*
+ * get 3 random categories and add films that correspond  
+ */
 getGenres(genre_url, function(){
     let genres = []
     for (let i = 0; i < 3; i++) { 
         random = genres_list[Math.floor(Math.random() * genres_list.length)]
         genres.push(random)        
     }
-    console.log("ran", genres)
-
 
     let categories = document.getElementById('categories')
     genres.forEach(function(genre) {
@@ -42,41 +55,42 @@ getGenres(genre_url, function(){
         newdiv.innerHTML=section
         categories.appendChild(newdiv)
 
-        let cat = document.getElementById(genre)
-        getGenreFilm(films_url + "?genre=" + genre, cat)
+        console.log("deq", genre)
+        getGenreFilm(films_url + "?genre="+ String(genre), String(genre), function(){})
 
     });
 })
 
-
+/************************************   Add films   ************************************/
 filmList = []
 let data = ""
-function getGenreFilm(url, cat){
+function getGenreFilm(url, idCategory, callback){
+    let categoryContainer = document.getElementById(idCategory)
     fetchUrl(url).then((res) => {
         
         for (let i = 0; i < res.results.length; i++) {
-            console.log("in",res.results[i].title )
             if (filmList.length === 7){
-               break
-            }    
-            
-            data += `
-            <div class="film__content">
-                <p class="title__category">${res.results[i].title}</p>
-                <img class="img__category" src="${res.results[i].image_url}" />
-            </div>` 
-            filmList.push(res.results[i])
-            
+                callback.call(this);
+                console.log("full")
+                console.log("de",filmList)
+                break
+            }else{  
+                console.log(" not full")
+                data += `
+                <div class="film__content">
+                    <p class="title__category">${res.results[i].title}</p>
+                    <img class="img__category" src="${res.results[i].image_url}" />
+                </div>` 
+                filmList.push(res.results[i])
+            } 
         }
-        cat.innerHTML = data
-
-        
-        console.log("in",filmList)
+        categoryContainer.innerHTML = data
+    
         if (filmList.length < 7){
             getGenreFilm(res.next)
         }
-        console.log("after loop", filmList)
     })  
+    
   
 }
 
@@ -86,6 +100,12 @@ function getGenreFilm(url, cat){
  * GET TOP 7 of a CATEGORY
  * ex: getGenreFilm(films_url + "?genre=action")
  */
+let bestFilmsUrl = "http://127.0.0.1:8000/api/v1/titles/?imdb_score_min=9"
+let bestFilmContainer = document.getElementById('best-films')
+let bestFilmList = []
+const topScore = 10
+getGenreFilm(bestFilmsUrl, "best-films", function(){})
+
 
 /** 2. 
  * GET TOP 7 of all film : random from diff category
@@ -126,12 +146,6 @@ function getGenreFilm(url, cat){
 // //}
 
 
-
-// // console.log('io',filmList[1].id);
-// // for (let i = 0; i < filmList.length; i++) { 
-// //     console.log("oo", filmList[1].id)
-    
-// //   }
 
 
 // console.log("DDD")
